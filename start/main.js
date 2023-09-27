b4c54e6ff8b12447b17519aa7a72fd2608d02d63
kosuke215135 <e215135@ie.u-ryukyu.ac.jp>
2023-09-27 16:16:21 +0900

    ベタ書きを回避

--- start/main.js
+++ start/main.js
@@ -1,27 +1,46 @@
+// CSVファイルを取得
+let csv = new XMLHttpRequest();
+ 
+// CSVファイルへのパス
+csv.open("GET", "../anime_data/title.csv", false);
+ 
+// csvファイル読み込み失敗時のエラー対応
+try {
+  csv.send(null);
+} catch (err) {
+  console.log(err);
+}
+ 
+// 配列を定義
+let csvArray = [];
+ 
+// 改行ごとに配列化
+let lines = csv.responseText.split(/\r\n|\n/);
+ 
+// 1行ごとに処理
+for (let i = 0; i < lines.length; ++i) {
+  let cells = lines[i].split(",");
+  if (cells.length != 1) {
+    csvArray.push(cells);
+  }
+}
+ 
+// コンソールに配列を出力
+console.log(csvArray);
+
 // 19枚の画像URLのリスト（実際のURLに置き換えてください）
-const imageUrls = [
-    "./image/animeimage/1.png",
-    "./image/animeimage/2.png",
-    "./image/animeimage/3.png",
-    "./image/animeimage/4.png",
-    "./image/animeimage/5.png",
-    "./image/animeimage/6.png",
-    "./image/animeimage/7.png",
-    "./image/animeimage/8.png",
-    "./image/animeimage/9.png",
-    "./image/animeimage/10.png"
-];
+const imageUrlsNum = csvArray.length - 1;
 
 function getRandomImages() {
     // ランダムに3つの画像を選択
     const randomImages = [];
     while (randomImages.length < 3) {
-        const randomIndex = Math.floor(Math.random() * imageUrls.length);
+        const randomIndex = Math.floor(Math.random() * imageUrlsNum);
         if (!randomImages.includes(randomIndex)) {
             randomImages.push(randomIndex);
         }
     }
-    return randomImages.map(index => imageUrls[index]);
+    return randomImages.map(index => '../anime_data/animeimage/' + index + '.png');
 }
 
 function loadRandomImages() {
